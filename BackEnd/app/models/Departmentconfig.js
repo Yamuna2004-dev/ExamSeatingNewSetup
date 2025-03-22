52b8e5418cb3b59adf6c2f17503bef335ec66685
Yamuna <yamunac2004@gmail.com>
2025-03-22 12:29:21 +0530

    BackEnd setup

---
+++ BackEnd/app/models/Departmentconfig.js
@@ -0,0 +1,26 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../../config/database');
+
+const Departmentconfig = sequelize.define('departmentconfig', {
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true
+		},
+		depname: DataTypes.STRING,
+		depid: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+  	},
+	{
+		indexes: [
+			// Create a unique index on email
+			{
+				unique: true,
+				fields: ['depid']
+			}],
+	});
+
+module.exports = Departmentconfig;
